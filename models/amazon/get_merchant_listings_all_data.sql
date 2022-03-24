af4f6f81ecd433f7ced3187a7ccbe3d8f27db429
Dat Tran <datitran@gmail.com>
2022-03-24 16:56:42 +0100

    chore: fix naming

--- models/amazon/get_merchant_listings_all_data.sql
+++ models/amazon/get_merchant_listings_all_data.sql
@@ -1,7 +1,7 @@
 with temp1 as (
     select
         {{ json_extract_scalar('_airbyte_data', ['item-name'], ['item-name']) }}::text as item_name
-    from {{ source('amazon', '_airbyte_raw_rounding_configs') }} as table_alias
+    from {{ source('amazon', '_airbyte_raw_get_merchant_listings_all_data') }} as table_alias
 )
 select 
     customer_id
