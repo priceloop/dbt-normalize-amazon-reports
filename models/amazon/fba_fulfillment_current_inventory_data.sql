c53c54f2302f0db6d33ccad95bfb55e24f88236f
bayoog <obaydaba96@gmail.com>
2022-07-09 12:01:27 +0200

    feat: update noramlization

--- models/amazon/fba_fulfillment_current_inventory_data.sql
+++ models/amazon/fba_fulfillment_current_inventory_data.sql
@@ -10,6 +10,8 @@ with temp2 as (
         {{ json_extract_scalar('_airbyte_data', ['fnsku'], ['fnsku']) }}::text as fnsku,
         {{ json_extract_scalar('_airbyte_data', ['country'], ['country']) }}::text as marketplace,
         -- {{ json_extract_scalar('_airbyte_data', ['seller-sku'], ['seller-sku']) }}::text as seller_sku,
+        {{ json_extract_scalar('_airbyte_data', ['snapshot-date'], ['snapshot-date']) }}::date as snapshot_date,
+        {{ json_extract_scalar('_airbyte_data', ['fulfillment-center-id'], ['fulfillment-center-id']) }}::text as fulfillment_center_id,
         nullif({{ json_extract_scalar('_airbyte_data', ['quantity'], ['quantity']) }}, '')::float as sellable_inventory
         
     from {{ source(
